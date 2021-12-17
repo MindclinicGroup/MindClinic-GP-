55727fc0caef41470b42642b0d881cf0335b5e30
GHAITH ALAWNEH <gaalawneh17@cit.just.edu.jo>
2021-12-17 19:39:15 +0300

    ShceduleTimesEdited

---
+++ MindClinic/Models/DoctorClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MindClinic.Models
+{
+    public class DoctorClass
+    {
+        public Guid ID { get; set; }
+
+    }
+}
